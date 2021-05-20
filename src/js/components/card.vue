152d076329785bec70b6153cee3a120fc418db22
Elroy <madcode.el@gmail.com>
2021-05-20 17:15:54 +0800

    add component card

---
+++ src/js/components/card.vue
@@ -0,0 +1,55 @@
+<template>
+  <a :href="link" class="card" :class="{
+    '-complete': !status
+  }">
+    <div class="card__logo">
+      <img :src="logo" alt="">
+    </div>
+    <div class="card__content">
+      <div class="card__content-info">
+        <p class="card__content-status">{{ statusText }}</p>
+        <p class="card__content-date" v-if="date && status">預計出貨{{ date }}</p>
+      </div>
+      <p class="card__title">{{ title }}</p>
+    </div>
+    <svg-icon name="right" icon-class="card__arrow"></svg-icon>
+  </a>
+</template>
+
+<script>
+import SvgIcon from './icon.vue'
+
+export default {
+  components: { SvgIcon },
+  props: {
+    link: {
+      type: String,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    statusText: {
+      type: String,
+      default: '',
+    },
+    logo: {
+      type: String,
+      default: '',
+    },
+    date: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 import '../styles/styles.scss'
+import './icons'
 import Vue from 'vue'
 import { mapActions, mapState } from 'vuex'
 import SvgIcon from './components/icon.vue'
+import Card from './components/card.vue'
 import store from './store/index'
 
 const app = new Vue({
-  el: "#app",
-  components: { SvgIcon }
+  el: '#app',
+  components: { SvgIcon, Card },
   store,
   methods: {
     ...mapActions(['UPDATE_DATA']),
